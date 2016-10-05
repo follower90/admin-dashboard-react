e01c12be566a64d25edac90ba722b59f0504522a
John Yani <vanuan@gmail.com>
2016-10-05 22:12:03 +0300

    Action creator example

---
+++ src/actionCreators.js
@@ -0,0 +1,9 @@
+export const TOGGLE_FOOTER_CLICK = 'TOGGLE_FOOTER_CLICK';
+
+const actionCreators = {
+  toggleFooterClick: () => ({
+    type: TOGGLE_FOOTER_CLICK
+  })
+};
+
+export default actionCreators;
