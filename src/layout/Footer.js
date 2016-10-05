46ad09800bf0b9b05a9e79c1a3a7d4445e84312e
John Yani <vanuan@gmail.com>
2016-10-05 22:31:14 +0300

    Call dispatcher from footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -2,14 +2,11 @@ import React, { Component } from 'react';
 import * as actions from './../Actions'
 
 export default class Footer extends Component {
-  toggleClick = () => {
-    actions.toggleFooterClick(!this.props.clicked)
-  }
 
   render() {
     let color = this.props.clicked ? 'green' : 'white'
     return (
-        <div className="footer" onClick={this.toggleClick}>
+        <div className="footer" onClick={this.props.toggleFooterClick}>
           <nav className="panel panel-default" style={{ backgroundColor: color }}>
             <div className="panel-body">
               {new Date().getFullYear()}
