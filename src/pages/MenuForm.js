0fc5b3fed87581d4b492ab047ffb75eb3a24b82c
Vitaliy Malyshev <vmalyshev@rocketfuel.com>
2017-08-12 22:18:32 +0300

    try recompose

--- src/pages/MenuForm.js
+++ src/pages/MenuForm.js
@@ -1,59 +1,47 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { compose, withState, lifecycle } from 'recompose'
 import { itemFetchData, saveMenuItem } from './../actions/MenuActionCreators'
 
-class MenuForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.props = props
-    this.state = this.props.item
+const mapStateToProps = state => ({ item: state.menuReducer.item });
+const enhance = compose(
+  withState('name', 'setName'),
+  withState('url', 'setUrl'),
+  lifecycle({
+    componentDidMount() {
+      this.props.dispatch(itemFetchData(this.props.routeParams.id))
+    },
+    componentWillReceiveProps(props) {
+      const { item, url, name }  = props;
+      this.setState({
+        name: name || item.name,
+        url: url || item.url,
+      })
+    }
+  }),
+)
 
-    this.changeName = this.changeName.bind(this);
-    this.changeUrl = this.changeUrl.bind(this);
-  }
-
-  changeName(event) {
-    this.setState({name: event.target.value});
-  }
-
-  changeUrl(event) {
-    this.setState({url: event.target.value});
-  }
-
-  componentDidMount() {
-    this.props.dispatch(itemFetchData(this.props.routeParams.id))
-  }
-
-  componentWillReceiveProps(props) {
-    this.setState(props.item);
-  }
-
-  render() {
+const MenuForm = enhance(({ name, url, setName, setUrl, history, dispatch }) => {
     return (
       <div>
           <h1>Меню edit page</h1>
-
           <div className="form">
               <input type="hidden" name="id" value="2" />
               <div className="form-group">
                   <label>Имя</label>
-                  <input className="form-control" name="name" onChange={this.changeName} value={this.state.name} />
+                  <input className="form-control" name="name" onChange={e => setName(e.target.value)} value={name} />
               </div>
               <div className="form-group">
                   <label>Ссылка</label>
-                  <input className="form-control" name="url"  onChange={this.changeUrl}  value={this.state.url} />
+                  <input className="form-control" name="url"  onChange={e => setUrl(e.target.value)} value={url} />
               </div>
-
-              <br />
-
               <div className="form-group">
-                  <input type="submit" className="btn btn-default" onClick={() => this.props.dispatch(saveMenuItem(this.state))} value="Сохранить" />
-                  <input type="button" className="btn btn-cancel" onClick={() => this.props.history.goBack()} value="Отмена" />
+                  <input type="submit" className="btn btn-default" onClick={() => dispatch(saveMenuItem({name: name, url: url}))} value="Сохранить" />
+                  <input type="button" className="btn btn-cancel" onClick={() => history.goBack()} value="Отмена" />
               </div>
           </div>
       </div>
     )
-  }
-}
+})
 
-export default connect(state => ({ item: state.menuReducer.item }))(MenuForm)
+export default connect(mapStateToProps)(MenuForm)
